fb8a28c75320e1e8f9ed7ba0ecf9ec537321c9b1
Barrientosgers <gersonbarrientos62@gmail.com>
2022-02-17 11:01:13 -0800

    php and replit added

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+    <title>PHP Page!</title>
+
+</head>
+
+<body>
+    <h1>PHP Page!</h1>
+    <p>This is HTML</p>
+    <?php
+        echo "<p>This is PHP</p>"
+    ?>
+
+
+
+</body>
+
+
+
+
+
+</html>
